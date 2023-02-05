f3b800950b8a65c4062864974c4dfe002b9fcb62
heispv <peymanvahidi1998@gmail.com>
2023-02-05 14:45:11 +0330

    update file

--- src/App.js
+++ src/App.js
@@ -4,112 +4,39 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkFrom from './components/ImageListForm/ImageLinkFrom';
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
 
 
 
+class App extends React.Component {
 
-const particlesOption = {
-  background: {
-      color: {
-          value: "#2d2d2d",
-      },
-  },
-  fpsLimit: 60,
-  interactivity: {
-      events: {
-          onClick: {
-              enable: true,
-              mode: "push",
-          },
-          onHover: {
-              enable: true,
-              mode: "repulse",
-          },
-          resize: true,
-      },
-      modes: {
-          push: {
-              quantity: 2,
-          },
-          repulse: {
-              distance: 200,
-              duration: 0.9,
-          },
-      },
-  },
-  particles: {
-      color: {
-          value: "#ffffff",
-      },
-      links: {
-          color: "#ffffff",
-          distance: 200,
-          enable: true,
-          opacity: 0.3,
-          width: 1,
-      },
-      collisions: {
-          enable: true,
-      },
-      move: {
-          directions: "none",
-          enable: true,
-          outModes: {
-              default: "bounce",
-          },
-          random: true,
-          speed: 4,
-          straight: false,
-      },
-      number: {
-          density: {
-              enable: true,
-              area: 700,
-          },
-          value: 100,
-      },
-      opacity: {
-          value: 0.7,
-      },
-      shape: {
-          type: "circle",
-      },
-      size: {
-          value: { min: 1, max: 5 },
-      },
-  },
-  detectRetina: true,
-}
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    }
+  }
 
-const App = () => {
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    await loadFull(engine);
-  }, []);
-  
-  const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-  }, []);
-  
-  return (
-    <div className="App">
-      <Particles className='particles' id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options= {particlesOption} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkFrom />
-      {/* <Logo />
-      <ImageLinkFrom />
-      <FaceRecognition /> */}
-    </div>
-  );
- 
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+    
+  render() {
+    return (
+        <div className="App">
+          <div className='particles'>
+            <ParticlesBackground />
+          </div>
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkFrom onInputChange={this.onInputChange}/>
+          {/* <Logo />
+          <ImageLinkFrom />
+          <FaceRecognition /> */}
+        </div>
+      );
+  }
 }
 
 export default App;
--- src/components/ImageListForm/ImageLinkFrom.js
+++ src/components/ImageListForm/ImageLinkFrom.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkFrom.css"
 
-const ImageLinkFrom = () => {
+const ImageLinkFrom = ({ onInputChange }) => {
   return (
     <div>
       <p className="white f3">
@@ -9,7 +9,7 @@ const ImageLinkFrom = () => {
       </p>
       <div className="inputs">
         <div className="inputAndButton pa4 shadow-5 br3" >
-          <input className="f4 pa2 w-75 center" type={"text"} />
+          <input className="f4 pa2 w-75 center" type={"text"} onChange={onInputChange}/>
           <button className="w-25 f4 grow link ph3 pv2 dib white bg-light-purple">Get the name</button>
         </div>
       </div>
